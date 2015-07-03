55ef5055645d9d5047861881b47e7d90c4446f96
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-03 20:04:06 +0200

    create methods for directory creation, file copying and config file creation and loading in infrastructure and remove boilerplate for these tasks from lib/syclink and use methods instead

--- lib/syclink/infrastructure.rb
+++ lib/syclink/infrastructure.rb
@@ -2,6 +2,28 @@ module SycLink
 
   module Infrastructure
 
+    # Creates a directory if it does not exist
+    def create_directory_if_missing(directory)
+      unless File.exists? directory
+        Dir.mkdir directory
+      end
+    end
+
+    # Copies a file to a target directory
+    def copy_file_if_missing(file, to_directory)
+      unless File.exists? File.join(to_directory, File.basename(file))
+        FileUtils.cp(file, to_directory)
+      end
+    end
+
+    # Loads the configuration from a file
+    def load_config(file)
+      unless File.exists? file
+        File.open(file, 'w') { |f| YAML.dump({ }, f) }
+      end
+      YAML.load_file(file)
+    end
+
     # Creates the filename of the website for saving, loading and deleting
     def yaml_file(directory = '.', website)
       File.join(directory, "#{website.downcase.gsub(/\s/, '-')}.website")
@@ -10,7 +32,7 @@ module SycLink
     # Creates an html filename to save the html representation of the website
     # to
     def html_file(directory = '.', website)
-      File.join(directory, "{website.downcase.gsub(/\s/, '-')}.html")
+      File.join(directory, "#{website.downcase.gsub(/\s/, '-')}.html")
     end
 
   end
