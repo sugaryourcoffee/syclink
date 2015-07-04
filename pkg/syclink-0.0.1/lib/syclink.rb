f6b5ce2bce397a78c4502a82219310fd06663529
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-04 02:07:52 +0200

    add pkg/ to .gitignore

--- pkg/syclink-0.0.1/lib/syclink.rb
+++
@@ -1,7 +0,0 @@
-require 'syclink/designer'
-require 'syclink/link'
-require 'syclink/exporter'
-require 'syclink/website'
-require 'syclink/infrastructure'
-require 'syclink/formatter'
-
--- pkg/syclink-0.0.1/lib/syclink/designer_example.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'designer'
-
-# Module that creates a link list and generates an html representation
-module SycLink
-  designer = Designer.new
-  designer.new_website("SYC LINK")
-  designer.add_links_from_file("./templates/links")
-  designer.create_website("./templates", "./templates/syclink.html.erb")
-end
