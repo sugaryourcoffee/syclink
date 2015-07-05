81d30e27cba4121326ee19b3c7f9b46ddd6539dc
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-06 00:36:02 +0200

    add importer for Firefox bookmarks

---
+++ lib/syclink/importer.rb
@@ -0,0 +1,43 @@
+require 'sqlite3'
+require_relative 'link'
+
+module SycLink
+
+  class Importer
+    
+    QUERY_STRING = "select p.url, p.title, b.title, a.content, k.keyword, b_t.title from moz_bookmarks b left outer join moz_keywords k on b.keyword_id = k.id left outer join moz_items_annos a on a.item_id = b.id left outer join moz_bookmarks b_t on b.parent = b_t.id join moz_places p on p.id = b.fk where p.url like 'http%';"
+
+    attr_reader :import
+
+    def initialize(data = [])
+      @import = data
+    end
+
+    def read(bookmark_file_path, query_string = QUERY_STRING)
+      bookmark_file = Dir.glob(File.expand_path(bookmark_file_path)).shift
+      raise "Did not find file #{bookmark_file_path}" unless bookmark_file
+
+      db = SQLite3::Database.new(bookmark_file)
+
+      import = db.execute(query_string)
+    end 
+
+    def rows
+      import.map do |row|
+        a = row[0]; b = row[1]; c = row[2]; d = row[3]; e = row[4]; f = row[5]
+        [a, 
+         b || c, 
+         (d || '').gsub("\n", ' '), 
+         [e, f].join(',').gsub(/^,|,$/, '')]
+      end
+    end
+
+    def links
+      rows.map do |row|
+        attributes = Link::ATTR
+        Link.new(row.shift, Hash[row.each { |v| [attributes.shift, v] }])
+      end
+    end
+  end
+
+end
---
+++ spec/syclink/importer_spec.rb
@@ -0,0 +1,23 @@
+require 'syclink/importer'
+
+module SycLink
+
+  describe Importer do
+
+    include Formatter
+
+    describe "should import Firefox bookmarks" do
+    
+      it "and create rows"
+      
+      it "and create links"
+
+    end
+
+    it "should import Internet Explorer bookmarks"
+
+    it "should import Chrome bookmarks"
+
+  end
+
+end
