cebd260bf2172940567fbc9bfed78962a0b8f147
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-07 23:37:44 +0200

    implement internet explorer bookmark import

--- lib/syclink/internet_explorer.rb
+++ lib/syclink/internet_explorer.rb
@@ -1,17 +1,23 @@
 require_relative 'importer'
 
+# Module that creates a link list and generates an html representation
 module SycLink
 
+  # Importer for Internet Explorer
   class InternetExplorer < Importer
 
+    # Reads the links from the Internet Explorer's bookmarks directory
     def read(path_to_internet_explorer_bookmarks)
       files = Dir.glob(File.join(path_to_internet_explorer_bookmarks, "**/*"))
-      files.map do |file|
-        [File.basename(file), File.dirname(file)] unless File.directory? file
-      end.compact
-    end
-
-    def rows
+      import = files.map do |file|
+        unless File.directory? file
+          url = File.read(file).scan(/(?<=\nURL=)(.*)$/).flatten.first
+          name = File.basename(file)
+          description = ""
+          tag         = File.dirname(file).gsub("/", ",")
+          [url, name, description, tag]
+        end.compact
+      end
     end
 
   end
