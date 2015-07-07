18ebd7b62b00881d4ab342b509285d909453713c
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-07 23:37:09 +0200

    fix link creation

--- lib/syclink/importer.rb
+++ lib/syclink/importer.rb
@@ -1,27 +1,36 @@
 require_relative 'link'
 
+# Module that creates a link list and generates an html representation
 module SycLink
 
+  # To be subclassed for link importers.
   class Importer
     
+    # Raw data of imported links
     attr_accessor :import
 
+    # Optionally loading raw data
     def initialize(data = [])
       @import = data
     end
 
+    # To be overridden! 
+    # Read the raw data from the bookmarks file.
     def read(bookmark_file_path)
       raise NotImplementedError
     end 
 
+    # Links values returned in an Array. Default implementation returns values
+    # from #read.
     def rows
-      raise NotImplementedError
+      import
     end
 
+    # Links returned as Link objects
     def links
       rows.map do |row|
-        attributes = Link::ATTR
-        Link.new(row.shift, Hash[row.each { |v| [attributes.shift, v] }])
+        attributes = Link::ATTRS.dup - [:url]
+        Link.new(row.shift, Hash[row.map { |v| [attributes.shift, v] }])
       end
     end
   end
