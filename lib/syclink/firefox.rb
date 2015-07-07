de19e3ca16202b4c4825206ab67e05cd7f3a17ba
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-07 23:38:05 +0200

    add comments

--- lib/syclink/firefox.rb
+++ lib/syclink/firefox.rb
@@ -1,12 +1,16 @@
 require 'sqlite3'
 require_relative 'importer'
 
+# Module that creates a link list and generates an html representation
 module SycLink
 
+  # Importer for Firefox links
   class Firefox < Importer
     
+    # Query strig to read links from the Firefox database places.sqlite
     QUERY_STRING = "select p.url, p.title, b.title, a.content, k.keyword, b_t.title from moz_bookmarks b left outer join moz_keywords k on b.keyword_id = k.id left outer join moz_items_annos a on a.item_id = b.id left outer join moz_bookmarks b_t on b.parent = b_t.id join moz_places p on p.id = b.fk where p.url like 'http%';"
 
+    # Reads the links from the Firefox database places.sqlite
     def read(bookmark_file_path)
       bookmark_file = Dir.glob(File.expand_path(bookmark_file_path)).shift
       raise "Did not find file #{bookmark_file_path}" unless bookmark_file
@@ -16,6 +20,7 @@ module SycLink
       import = db.execute(QUERY_STRING)
     end 
 
+    # Returns row values in Arrays
     def rows
       import.map do |row|
         a = row[0]; b = row[1]; c = row[2]; d = row[3]; e = row[4]; f = row[5]
