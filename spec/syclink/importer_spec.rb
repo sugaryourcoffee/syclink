aa539880f96f3b04eb1095b96393650562ff9c09
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-07 23:38:25 +0200

    finish firefox spec

--- spec/syclink/importer_spec.rb
+++ spec/syclink/importer_spec.rb
@@ -10,17 +10,25 @@ module SycLink
         @import = [ [ "http://a.com", "a", "at", "a site", nil,  "aa" ],
                     [ "http://b.com", "b", "bt", nil,      "ab", "bb" ],
                     [ "http://c.com", nil, "ct", "b site", "bc", nil  ] ] 
+        
+        @result = [ [ "http://a.com", "a",  "a site", "aa"    ],
+                    [ "http://b.com", "b",  "",       "ab,bb" ],
+                    [ "http://c.com", "ct", "b site", "bc"    ] ]
+
         @firefox = Firefox.new(@import)
       end
 
       it "and create rows" do
-        result = [ [ "http://a.com", "a",  "a site", "aa"    ],
-                   [ "http://b.com", "b",  "",       "ab,bb" ],
-                   [ "http://c.com", "ct", "b site", "bc"    ] ]
-        expect(@firefox.rows).to eq  result
+        expect(@firefox.rows).to eq  @result
       end
       
-      it "and create links"
+      it "and create links" do
+        links = @firefox.links
+        expect(links.first.url).to         eq @result.first[0]
+        expect(links.first.name).to        eq @result.first[1]
+        expect(links.first.description).to eq @result.first[2]
+        expect(links.first.tag).to         eq @result.first[3]
+      end
 
     end
 
