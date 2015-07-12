183faa8afd48977250181960eef3db7e0d8177d4
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-07-12 22:17:35 +0200

    add requires for import files

--- lib/syclink.rb
+++ lib/syclink.rb
@@ -5,4 +5,10 @@ require 'syclink/exporter'
 require 'syclink/website'
 require 'syclink/infrastructure'
 require 'syclink/formatter'
+require 'syclink/importer'
+require 'syclink/firefox'
+require 'syclink/chrome'
+require 'syclink/internet_explorer'
+require 'syclink/file_importer'
+
 
